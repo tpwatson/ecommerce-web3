d2b41b8e706d32d2ae3cd2e92c9797b726949dc9
tpwatson <terence.p.watson@gmail.com>
2025-10-03 14:34:26 -0700

    basic header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <>
     <ul className="flex flex-col gap-4">
-      <li>Header</li>
       <li>Hero Section</li>
       <li>CategoryDisplay</li>
       <li>Footer</li>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <>
-        <span>Footer Links and Socials</span>
-        </>
+        <footer className="absolute bottom-0 w-full bg-white/0.05">
+            <ul className="flex flex-row justify-around items-center h-16">
+                <li>X Community</li>
+                <li>PumpFun Stream Link</li>
+                <li>Telegram Community</li>
+            </ul>
+        </footer>
+
     )
 }
